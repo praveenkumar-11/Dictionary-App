3312d57e80ab48fc5dea4517fc70aa61b97ce595
praveenkumar-11 <praveenkumar10082003@gmail.com>
2023-08-25 08:33:57 +0530

    Drop Down and Toggle Updated

--- App/Scripts/script.js
+++ App/Scripts/script.js
@@ -1,8 +1,10 @@
 //import fontss from "App/Styles/_fonts.scss";
 
 const drop_down= document.querySelector(".current-font-and-icon");
+const current_font= document.querySelector(".current-font");
 const drop_down_con= document.querySelector(".drop-down-content");
 const fonts= document.querySelectorAll(".drop-down-content span");
+const doc= document.querySelectorAll("*");
 const font_styles={
     serif: "'Lora', serif",
     sans_serif: "'Inter', sans-serif",
@@ -21,7 +23,9 @@ drop_down.addEventListener("blur", ()=>{
 
 for(let i=0; i<fonts.length; i++){
     fonts[i].addEventListener("click", (event)=>{
-        //.style.fontFamily= font_fam[i];
-        console.log(font_fam[i]);
+        for(let j=0; j<doc.length; j++){
+            doc[j].style.fontFamily= font_fam[i];
+        }
+        current_font.innerHTML= fonts[i].innerHTML;
     })
 }
